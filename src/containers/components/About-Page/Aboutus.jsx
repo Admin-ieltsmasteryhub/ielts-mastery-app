5da23286895c6eaeab21d1a0da513ccbd0844a02
Admin-ieltsmasteryhub <ieltsmasteryhub@gmail.com>
2023-09-15 20:32:52 +0530

    updated button and page nav number button

--- src/containers/components/About-Page/Aboutus.jsx
+++ src/containers/components/About-Page/Aboutus.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./aboutus.css";
+import practicecommonstyles from '../Practice/practiceCommon.module.css'
 
 const AboutExam = ({ title, updateDate, author, image, article }) => {
     return (
         <div className='container'>
-      <div className="article-page">
-        <div className="container_a">
-            <div className='book_test'>
+      <div className={practicecommonstyles.practice_page_container}>
+        <div className={practicecommonstyles.practice_page_child1}>
+            <div className={practicecommonstyles.practice_page_title}>
           <h1>{title}</h1>
           </div>
           <p>Updated on {updateDate} by {author}</p>
@@ -14,7 +15,7 @@ const AboutExam = ({ title, updateDate, author, image, article }) => {
 <div className="article-content"  dangerouslySetInnerHTML={{ __html: article }}>
 </div>
         </div>
-        <div className="related-container">
+        <div className={practicecommonstyles.practice_page_child2}>
           <h2>Related Posts</h2>
           {/* Add your related posts content here */}
         </div>
--- src/containers/components/Vocabulary/SynonymsAntonyms/SynonymsAntonyms.jsx
+++ src/containers/components/Vocabulary/SynonymsAntonyms/SynonymsAntonyms.jsx
@@ -1,11 +1,13 @@
+
 import React, { useState } from 'react';
 import './synonymsantonyms.css';
-import vocabularycommonstyles from '../vocabularyCommon.module.css'
+import vocabularycommonstyles from '../vocabularyCommon.module.css';
 
 const SynonymsAntonymsPage = () => {
   const [results, setResults] = useState(dummyResults); // Replace with your API data
   const resultsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
+  const pagesToShow = 5; // Number of pages to show in the pagination
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -15,14 +17,30 @@ const SynonymsAntonymsPage = () => {
   const endIndex = startIndex + resultsPerPage;
   const currentResults = results.slice(startIndex, endIndex);
 
+  // Calculate the range of page numbers to display in the pagination
+  const totalPages = Math.ceil(results.length / resultsPerPage);
+  const halfPagesToShow = Math.floor(pagesToShow / 2);
+  let startPage = Math.max(currentPage - halfPagesToShow, 1);
+  let endPage = Math.min(startPage + pagesToShow - 1, totalPages);
+
+  // Adjust the startPage if there are not enough pages to show
+  if (endPage - startPage + 1 < pagesToShow) {
+    startPage = Math.max(endPage - pagesToShow + 1, 1);
+  }
+
+  // Create an array of page numbers to show in the pagination
+  const pageNumbers = Array.from({ length: endPage - startPage + 1 }, (_, index) =>
+    startPage + index
+  );
+
   return (
     <div className='container'>
-    <div className={vocabularycommonstyles.vocabulary_page}>
-    <div className={vocabularycommonstyles.vocabulary_page_container}>
-    <div className="synonyms-antonyms-page">
-      <h1>Synonyms and Antonyms</h1>
-      <table className="results-table">
-        <thead>
+      <div className={vocabularycommonstyles.vocabulary_page}>
+        <div className={vocabularycommonstyles.vocabulary_page_container}>
+          <div className="synonyms-antonyms-page">
+            <h1>Synonyms and Antonyms</h1>
+            <table className="results-table">
+            <thead>
           <tr>
             <th>Word</th>
             <th>Synonyms</th>
@@ -38,21 +56,21 @@ const SynonymsAntonymsPage = () => {
             </tr>
           ))}
         </tbody>
-      </table>
-      <div className="pagination">
-        {Array.from({ length: Math.ceil(results.length / resultsPerPage) }).map((_, index) => (
-          <button
-            key={index}
-            className={index + 1 === currentPage ? 'active' : ''}
-            onClick={() => handlePageChange(index + 1)}
-          >
-            {index + 1}
-          </button>
-        ))}
+            </table>
+            <div className="pagination">
+              {pageNumbers.map((pageNumber) => (
+                <button
+                  key={pageNumber}
+                  className={pageNumber === currentPage ? 'active' : ''}
+                  onClick={() => handlePageChange(pageNumber)}
+                >
+                  {pageNumber}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    </div>
     </div>
   );
 };
@@ -62,6 +80,283 @@ export default SynonymsAntonymsPage;
 const dummyResults = [
   // Sample data
   {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },
+  {
+    word: 'Happy',
+    synonyms: ['Joyful', 'Content', 'Ecstatic'],
+    antonyms: ['Sad', 'Unhappy', 'Miserable'],
+  },  {
     word: 'Happy',
     synonyms: ['Joyful', 'Content', 'Ecstatic'],
     antonyms: ['Sad', 'Unhappy', 'Miserable'],
