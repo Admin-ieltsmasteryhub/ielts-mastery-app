1c9ddd5422a8437c5891eac613588ceb2ec84cd5
Admin-ieltsmasteryhub <137681726+Admin-ieltsmasteryhub@users.noreply.github.com>
2023-10-14 00:46:35 +0530

    Update Courses.jsx

--- src/containers/components/Courses-section/Courses.jsx
+++ src/containers/components/Courses-section/Courses.jsx
@@ -3,9 +3,10 @@ import { Container, Row, Col } from "reactstrap";
 import courseImg1 from "../../assets/images/web-design.png";
 import courseImg2 from "../../assets/images/graphics-design.png";
 import courseImg3 from "../../assets/images/ui-ux.png";
-import styles from "./courses-section.module.css";
+import styles from "./courses.module.css";
 import CourseCard from "./CourseCard";
-
+import { HiArrowRight } from 'react-icons/hi';
+import { NavLink } from "react-router-dom";
 const coursesData = [
   {
     id: "01",
@@ -15,7 +16,6 @@ const coursesData = [
     rating: 5.9,
     imgUrl: courseImg1,
   },
-
   {
     id: "02",
     title: "Professional Graphics Design, PhotoShop, Adobe XD, Figma",
@@ -24,9 +24,16 @@ const coursesData = [
     rating: 5.9,
     imgUrl: courseImg2,
   },
-
   {
     id: "03",
+    title: "UI/UX BootCamp for Beginners in 2022",
+    lesson: 12,
+    students: 12.5,
+    rating: 5.9,
+    imgUrl: courseImg3,
+  },
+  {
+    id: "04",
     title: "UI/UX BootCamp for Beginners in 2022",
     lesson: 12,
     students: 12.5,
@@ -38,33 +45,22 @@ const coursesData = [
 const Courses = () => {
   return (
     <section>
-      <Container>
-        <Row>
+      <Container fluid>
+        <Row >
           <div className={styles.centered_content}>
-      <h2>Our Popular Courses</h2>
-      <p>
-      Unlock your potential with IELTS Mastery Hub. Achieve success in the IELTS exam through expert guidance, comprehensive resources, and personalized support. Start your journey today.
-      </p>
-      </div>
-          <Col lg="12" className="mb-5">
-            <div className="course__top d-flex justify-content-between align-items-center">
-              {/* <div className="course__top__left w-50">
-                <h2>Our Popular Courses</h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-                  consequatur libero quod voluptatibus ullam quia quas, vitae
-                  voluptatem recusandae reprehenderit!
-                </p>
-              </div> */}
-
-              <div className="course__btn w-50 text-end">
-                <button className="btn">See All</button>
-              </div>
-            </div>
+            <h2>Our Popular Courses</h2>
+            <p>
+              Unlock your potential with IELTS Mastery Hub. Achieve success in
+              the IELTS exam through expert guidance, comprehensive resources,
+              and personalized support. Start your journey today.
+            </p>
+          </div>
+          <Col lg="12" className={styles.course_all_link}>
+            <NavLink to={'/dictionary'}>View All <HiArrowRight></HiArrowRight></NavLink>
           </Col>
           {coursesData.map((item) => (
-            <Col lg="4" md="6" sm="6">
-              <CourseCard key={item.id} item={item} />
+            <Col lg="3" md="8" sm="8" key={item.id}>
+              <CourseCard item={item} />
             </Col>
           ))}
         </Row>
