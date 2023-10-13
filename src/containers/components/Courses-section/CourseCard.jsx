7f212be4bfcc3ff5c645afc42ca28d5bc72993ec
Admin-ieltsmasteryhub <137681726+Admin-ieltsmasteryhub@users.noreply.github.com>
2023-10-14 00:46:03 +0530

    Update CourseCard.jsx

--- src/containers/components/Courses-section/CourseCard.jsx
+++ src/containers/components/Courses-section/CourseCard.jsx
@@ -1,33 +1,34 @@
 import React from "react";
+import styles from './coursescard.module.css'
 
 const CourseCard = (props) => {
   const { imgUrl, title, lesson, students, rating } = props.item;
 
   return (
-    <div className="single__course__item">
-      <div className="course__img">
+    <div className={styles.single_course_item}>
+      <div className={styles.course_img}>
         <img src={imgUrl} alt="" className="w-100" />
       </div>
 
-      <div className="course__details">
-        <h6 className="course__title mb-4">{title}</h6>
+      <div className={styles.course_details}>
+        <h6 className={styles.course_title}>{title}</h6>
 
-        <div className=" d-flex justify-content-between align-items-center">
-          <p className="lesson d-flex align-items-center gap-1">
+        <div className="d-flex justify-content-between align-items-center">
+          <p className={`${styles.lesson} d-flex align-items-center gap-1`}>
             <i className="ri-book-open-line"></i> {lesson} Lessons
           </p>
 
-          <p className="students d-flex align-items-center gap-1">
+          <p className={`${styles.students} d-flex align-items-center gap-1`}>
             <i className="ri-user-line"></i> {students}K
           </p>
         </div>
 
-        <div className=" d-flex justify-content-between align-items-center">
-          <p className="rating d-flex align-items-center gap-1">
+        <div className="d-flex justify-content-between align-items-center">
+          <p className={`${styles.rating} d-flex align-items-center gap-1`}>
             <i className="ri-star-fill"></i> {rating}K
           </p>
 
-          <p className="enroll d-flex align-items-center gap-1">
+          <p className={`${styles.enroll} d-flex align-items-center gap-1`}>
             <a href="facebook.com"> Enroll Now</a>
           </p>
         </div>
